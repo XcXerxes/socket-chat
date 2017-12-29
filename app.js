01ec2de4304a93720b930b2c738d70df04a7b7e7
Smile <xcxerxes@aliyun.com>
2017-12-29 18:15:15 +0800

    修改顶端路由

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'static')))
-app.use('/', routes)
+app.use('/chat', routes)
 
 
 const port = process.env.PORT || 8888
